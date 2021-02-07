14a1f05beca09cd9b0fa212da7022ff999cbde64
prashant <prashantnk2001@gmail.com>
2021-02-08 02:05:58 +0530

    correct mailer.js

--- mailer.js
+++ mailer.js
@@ -6,9 +6,8 @@ const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
 const redirectUri = process.env.REDIRECT_URI;
 const RefreshToken = process.env.REFRESH_TOKEN;
-const oauthClient = new google.auth.OAuth2(clientId , clientSecret , redirectUri);
 const user = process.env.USERMAILER;
-oauthClient.setCredentials({refresh_token: RefreshToken});
+
 function sendmail(accessToken , email , msg)
 {
     const transporter = nodemailer.createTransport({
@@ -30,6 +29,8 @@ function sendmail(accessToken , email , msg)
     });
 }
 exports.mail = (email , text )=>{
+    const oauthClient = new google.auth.OAuth2(clientId , clientSecret , redirectUri);
+    oauthClient.setCredentials({refresh_token: RefreshToken});
     oauthClient.getAccessToken().then((found)=>{
         sendmail(found.res.data.access_token , email , text);
     }).catch(error => { throw error});
